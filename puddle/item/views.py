e486fd60c7a2849929d5c8ea492d652f19fc87b8
Erdem Inmez <inmez@sheridancollege.ca>
2023-09-17 19:55:54 -0400

    Search Item Added

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 
-from .models import Item
+from .models import Item, Category
 from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
@@ -56,4 +57,23 @@ def edit(request, pk):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'Edit item',
+    })
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
     })
