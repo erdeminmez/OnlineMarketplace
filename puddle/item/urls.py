8558fef113a215ba6251c0780f88dd540e81e048
Erdem Inmez <inmez@sheridancollege.ca>
2023-09-17 19:26:19 -0400

    Delete Item Added

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -7,4 +7,5 @@ app_name="item"
 urlpatterns = [
     path('<int:item_id>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
+    path('<int:pk>/delete', views.delete, name='delete'),
 ]
