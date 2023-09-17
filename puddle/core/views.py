c57a26af51311ab3640a18de6fca1e0fc053ba86
Erdem Inmez <inmez@sheridancollege.ca>
2023-09-17 16:40:12 -0400

    Sign Up Added

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from item.models import Category, Item
+
+from .forms import SignupForm
 
 # Create your views here.
 def index(request):
@@ -13,3 +17,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return HttpResponseRedirect(reverse("login")) 
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
