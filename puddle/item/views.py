99df89222ca27866ca0ab3eebfd28990a8f4f434
Erdem Inmez <inmez@sheridancollege.ca>
2023-09-17 16:15:29 -0400

    Item Detail App Added

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Item
 
 # Create your views here.
+def detail(request, item_id):
+    item = get_object_or_404(Item, pk=item_id)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item_id)[0:3]
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
