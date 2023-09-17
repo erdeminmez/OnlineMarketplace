4c740aca4031152cd81d20ebd4447c999e546073
Erdem Inmez <inmez@sheridancollege.ca>
2023-09-17 19:36:22 -0400

    Edit Item Added

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name="item"
 
 urlpatterns = [
-    path('<int:item_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.detail, name='detail'),
     path('new/', views.new, name='new'),
-    path('<int:pk>/delete', views.delete, name='delete'),
+    path('<int:pk>/delete/', views.delete, name='delete'),
+    path('<int:pk>/edit/', views.edit, name='edit'),
 ]
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
-def detail(request, item_id):
-    item = get_object_or_404(Item, pk=item_id)
-    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=item_id)[0:3]
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items
@@ -38,3 +38,22 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
